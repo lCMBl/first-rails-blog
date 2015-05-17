14f21d4f58df84c4c49a30b8dcf13253d37116d0
Christian Barentine <cbarentine@gmail.com>
2015-05-17 17:32:51 -0500

    updated password

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "cmb", password: "moo", only: :destroy
+  http_basic_authenticate_with name: "cmb", password: BCrypt::Password.new("$2a$10$mKGkhoSq56fl/3oePef/jeN22IgjE8IR8wb.tdbbOui2UXBEXYIHC"), only: :destroy
 
   def create
     @post = Post.find(params[:post_id])
