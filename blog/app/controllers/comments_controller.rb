823fa540de047414a31d2df19bbd3ce8c8489d27
Christian Barentine <cbarentine@gmail.com>
2015-05-17 17:06:05 -0500

    added basic user authentication NO REALLY FIX THIS

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "cmb", password: "moo", only: :destroy
 
   def create
     @post = Post.find(params[:post_id])
@@ -15,5 +16,13 @@ class CommentsController < ApplicationController
 private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
+  end
+
+  # "$2a$10$XJIUrXJizGqXpM/FbCJ.h.wqKuBMLX9.567wEqhwiHo7fgEMjiaVG"
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      passHash = BCrypt::Password.new("$2a$10$XJIUrXJizGqXpM/FbCJ.h.wqKuBMLX9.567wEqhwiHo7fgEMjiaVG")
+      session[:admin] = (user_name == "cmb" && passHash == password)
+    end
   end
 end
